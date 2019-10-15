cad8201b15227c62d197d98566e5716b38c9ab83
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-10-15 12:16:19 -0600

    Adds SQL

---
+++ setup.sql
@@ -0,0 +1,32 @@
+USE burgershack6;
+-- CREATE TABLE burgers
+-- (
+--     id VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     price decimal(5,2) DEFAULT .99,
+--     description VARCHAR(255) NOT NULL,
+
+--     PRIMARY KEY(id)
+-- );
+
+-- CREATE
+-- INSERT INTO burgers 
+-- (id, name, price, description)
+-- VALUES
+-- ("235", "D$", 12.99, "All the meat and cheese we have");
+
+-- GET ALL
+-- SELECT * FROM burgers;
+
+-- GET BY (Find)
+-- SELECT * FROM burgers WHERE id = 123;
+
+
+-- EDIT
+-- UPDATE burgers
+-- SET name = "Aloha Zach"
+-- WHERE id = 123;
+
+-- REMOVE by
+-- DELETE FROM burgers WHERE id = 235;
+
