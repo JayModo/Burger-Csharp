8d7e4966660289bda9b68350111bbfa3a7ceae43
Jeremy O <galaxyjay123@gmail.com>
2019-10-16 16:51:26 -0600

    deleted fake DB

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BurgerShack.Db;
+
 using BurgerShack.Models;
 using BurgerShack.Services;
 using Fall_BurgerShack.Models;
